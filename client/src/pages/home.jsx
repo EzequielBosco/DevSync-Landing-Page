5200b9a8ec34d5623dd8291ba9f95c24e2ff481a
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2023-12-13 14:42:08 -0300

    add social icons, responsive design, vertical nav and logo

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,19 +1,37 @@
+import logo from '../assets/Logo.png'
+import { ImLinkedin } from "react-icons/im";
+import { IoLogoGithub } from "react-icons/io";
 import './home.css'
 
 const Home = () => {
   return(
     <>
       <main> 
-        <h1 id='title'>DEVSYNC SOLUTIONS</h1>
-        <div className='div-home'>
-          <div class="loading">
-            <span class="carga"></span>
+        <div className='left'>
+          <div className='div-name'>
+            <img src={logo} alt='Logo Devsync' />
+            <h1 className='vertical-text'>DEVSYNC</h1>
           </div>
-          <h2>Sitio en Mantenimiento...</h2>
+          <div className='div-media'>
+            <a href='https://www.linkedin.com/company/devsync-solutions/' target='_blank'>
+              <ImLinkedin />
+            </a>
+            <a href='https://github.com/DevSync-Solutions' target='_blank'>
+              <IoLogoGithub />
+            </a>
+          </div>
         </div>
-          <h3>Por favor, vuelva más tarde</h3>
-        <br />
-        <span class="loader"></span>
+        <div className='right'>   
+          <div className='div-home'>
+            <div className="loading">
+              <span className="carga"></span>
+            </div>
+            <h2>Mantenimiento...</h2>
+          </div>
+            <h3>Por favor, vuelva más tarde</h3>
+          <br />
+          <span className="loader"></span>
+        </div>
       </main>
     </>
   )
